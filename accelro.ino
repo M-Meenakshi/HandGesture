300366cbbd63aa305fee3f9e0d97db6f23000b6f
M-Meenakshi <48921766+M-Meenakshi@users.noreply.github.com>
2019-08-26 17:17:19 +0530

    Add files via upload

---
+++ accelro.ino
@@ -0,0 +1,33 @@
+const int groundpin = 18;             // analog input pin 4 -- ground
+const int powerpin = 19;              // analog input pin 5 -- voltage
+const int xpin = A3;                  // x-axis of the accelerometer
+const int ypin = A2;                  // y-axis
+const int zpin = A1;                  // z-axis (only on 3-axis models)
+
+void setup() {
+  // initialize the serial communications:
+  Serial.begin(9600);
+
+  // Provide ground and power by using the analog inputs as normal digital pins.
+  // This makes it possible to directly connect the breakout board to the
+  // Arduino. If you use the normal 5V and GND pins on the Arduino,
+  // you can remove these lines.
+  pinMode(groundpin, OUTPUT);
+  pinMode(powerpin, OUTPUT);
+  digitalWrite(groundpin, LOW);
+  digitalWrite(powerpin, HIGH);
+}
+
+void loop() {
+  
+  Serial.print(analogRead(xpin));
+ 
+  Serial.print("\t");
+  Serial.print(analogRead(ypin));
+  
+  Serial.print("\t");
+  Serial.print(analogRead(zpin));
+  Serial.println();
+ 
+  delay(2000);
+}
